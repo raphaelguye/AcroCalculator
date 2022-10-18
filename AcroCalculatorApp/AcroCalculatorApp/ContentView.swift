372e24084a45eb1259b244898cf9f0fa680e084b
Raphael Guye <raphael.guye@gmail.com>
2022-10-18 15:18:37 +0200

    Remove 'App' project and create a new 'AcroCalculatorApp' to try to have a unique name for Xcode Cloud

---
+++ AcroCalculatorApp/AcroCalculatorApp/ContentView.swift
@@ -0,0 +1,18 @@
+import SwiftUI
+import MCFreestyle
+
+// MARK: - ContentView
+
+struct ContentView: View {
+  var body: some View {
+    MCFreestyleRootView()
+  }
+}
+
+// MARK: - ContentView_Previews
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+}
---
+++ AcroCalculatorApp/AcroCalculatorAppTests/AcroCalculatorAppTests.swift
@@ -0,0 +1,9 @@
+import XCTest
+
+final class AppTests: XCTestCase {
+
+  func testExample() throws {
+    XCTAssertTrue(true)
+  }
+
+}
